165ed3416c515255bc27bc050c14e00975a4834b
Kseniia <kceniawhite@gmail.com>
2021-12-25 19:34:27 +0200

    add new correction

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
 
+    const startBtn = document.querySelector('button, data-start')
+    const stopBtn = document.querySelector('button, data-stop')
+    const bodyEl = document.querySelector('body')
+
+    startBtn.addEventListener('click', onStartBtnClick)
+
+
+    // const timerId = setTimeout(onStartBtnClick, 1000)
+
+const onStartBtnClick = () => {
+    setTimeout(() =>{
+        bodyEl.style.backgroundColor = getRandomHexColor()},
+1000)
+    }
+
+    function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
