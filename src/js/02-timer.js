e90c180ff0cefffadf27056247a83a8af0d43977
Kseniia <kceniawhite@gmail.com>
2021-12-29 00:58:50 +0200

    final edition

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,14 @@
   import flatpickr from 'flatpickr';
   import 'flatpickr/dist/flatpickr.min.css';
 
-const days = document.querySelector('[data-days]')
-const hours = document.querySelector('[data-hours]')
-const mins = document.querySelector('[data-minutes]')
-const secs = document.querySelector('[data-seconds]')
+const daysRefs = document.querySelector('[data-days]')
+const hoursRefs = document.querySelector('[data-hours]')
+const minutesRefs = document.querySelector('[data-minutes]')
+const secondsRefs = document.querySelector('[data-seconds]')
  const valueEl = document.querySelector('.value')
 
 const startBtn = document.querySelector('button[data-start]')
  const inputEl = document.querySelector('#datetime-picker')
-  // inputEl.addEventListener('input', updateTimerValue);
    
 let selectedTime = 0;
 const options =  {
@@ -35,53 +34,49 @@ flatpickr(inputEl, options);
 
 startBtn.addEventListener('click', onBtnClick)
 
-      function onBtnClick() {
-        startBtn.disabled = true;
-          // const endTime = Date.now()
+function onBtnClick() {
+  startBtn.disabled = true;
+  // const selectedTime = Date.now()
 
-        setInterval(() => {
-          const currentTime = Date.now()
-          console.log(currentTime);
-          const counteTime = selectedTime.getTime() - currentTime
-          console.log(convertMs(counteTime));
-        const  { days, hours, mins, secs } = convertMs(counteTime)
-         updateTimerValue({days, hours, mins, secs})
-          console.log(`${days}:${hours}:${mins}:${secs}`);
-      }, 1000)
+  setInterval(() => {
+    const currentTime = Date.now();
+    const countTime = selectedTime.getTime() - currentTime;
+    // console.log(convertMs(counteTime));
+    const { days, hours, minutes, seconds } = convertMs(countTime);
+    // updateTimerValue({days, hours, minutes, seconds})
+    // console.log(`${days}:${hours}:${minutes}:${seconds}`);
 
+  daysRefs.textContent = `${days}`;
+hoursRefs.textContent = `${hours}`;
+minutesRefs.textContent = `${minutes}`;
+secondsRefs.textContent = `${seconds}`;
+  }, 1000) 
+       
 }
-// const timer = {
-//     isActive: false,
-//     end() {
-//         if (this.isActive) {
-//         return
-//       }
-    
-//    },
-// }
 
-function updateTimerValue({days, hours, mins, secs}) {
-valueEl.textContent = `${days}:${hours}:${mins}:${secs}`
-};
-// pad(value) {
-//   return String(value).padStart(2, '0')
-// }
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+// function updateTimerValue({days, hours, minutes, seconds}) {
+//   valueEl.textContent = `${days}:${hours}:${minutes}:${seconds}`
+// };
+function pad(value) {
+  return String(value).padStart(2, '0')
+}
 
-    // Remaining days
-    const days = Math.floor(ms / day);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = pad(Math.floor((ms % day) / hour)) ;
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute)) ;
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
   }
